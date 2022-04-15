25dfb095464237c6892f8e173fbf0a9b06211895
pool <loop.edward@gmail.com>
2022-04-15 17:56:46 +0800

    Configure local dynamodb

--- handler/menu/get.js
+++ handler/menu/get.js
@@ -1,23 +1,55 @@
-const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+// const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDB } = require("aws-sdk");
 
-const client = new DynamoDBClient({ region: process.env.DEFAULT_REGION_NAME });
+// const dynamoDbClientParams = {};
+// if (process.env.IS_OFFLINE) {
+//     dynamoDbClientParams.region = 'local'
+//     dynamoDbClientParams.endpoint = 'http://localhost:8000'
+//     dynamoDbClientParams.accessKeyId = 'DEFAULT_ACCESS_KEY'  // needed if you don't have aws credentials at all in env
+//     dynamoDbClientParams.secretAccessKey = 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
+// }
+// console.log("CLIENT PARAMS:", dynamoDbClientParams);
+// const dynamoDbClient = new AWS.DynamoDB.DocumentClient(dynamoDbClientParams);
+
+const db = new DynamoDB({
+    region: 'local',
+    endpoint: 'http://localhost:8000'
+});
+const TableName = process.env.LESQ_TABLE;
 
 module.exports.handler = async (event, context) => {
-    console.log("Event:", event);
+    const { merchantId } = event.pathParameters;
 
-    const command = new PutItemCommand({
-        TableName: process.env.LESQ_TABLE,
-        Item: {
-            PK: {S:"PK1"},
-            SK: {S:"SK1"}
-        }
-    });
+    console.log("Obtaining menu for merchant: ", merchantId);
+    // const command = new PutItemCommand({
+    //     TableName,
+    //     Item: {
+    //         PK: { S: "PK2" },
+    //         SK: { S: "SK2" }
+    //     }
+    // });
     try {
-        const response = await client.send(command);
+        // const response = await dynamoDbClient.send(command);
+        const response = await db.putItem({
+            TableName,
+            Item: {
+                PK: { S: "PK3" },
+                SK: { S: "SK3" },
+                Bubot: { S: "KLaon" },
+            }
+        }).promise();
         console.log("Success", response);
+        const x = await db.getItem({
+            Key: {
+                "PK": { S: "PK3" },
+                "SK": { S: "SK3" }
+            },
+            TableName
+        }).promise();
+        console.log(x);
         return {
             statusCode: 200,
-            body: JSON.stringify({ text: "success" })
+            body: JSON.stringify(x)
         };
     } catch (error) {
         console.log("Error: ", error);
